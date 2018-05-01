221de67a03fea685506aa2e6117bbb9778feab1a
Daniel Seagraves <dseagrav@lunar-tokyo.net>
2018-05-01 04:37:08 -0500

    Fix help message, add license/copyright

--- tools/disktool.c
+++ tools/disktool.c
@@ -1,4 +1,24 @@
-/* Lambda disk tool */
+/* Lambda disk tool
+
+   Copyright 2016-2018
+   Daniel Seagraves <dseagrav@lunar-tokyo.net>
+   Barry Silverman <barry@disus.com>
+
+   This file is part of LambdaDelta.
+
+   LambdaDelta is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   LambdaDelta is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LambdaDelta.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -311,7 +331,8 @@ int main(int argc, char *argv[]){
     printf("  print      Prints the disk label(s) and partitition table, if present\n");
     printf("  read       Reads the given partition into the given file\n");
     printf("             Parameters: (partition name) (partition image file name)\n");
-    printf("  write      Reads the given partition into the given file\n");
+    printf("  write      Writes the given file into the given partition, optionally setting the comment\n");
+    printf("             The remainder of the partition will be zeroed\n");
     printf("             Parameters: (partition image file name) (partition name) [partition comment]\n");
     return(0);
   }
